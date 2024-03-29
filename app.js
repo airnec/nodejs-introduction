629e2b3e7e63f8410ee216ab28a23e51ae69eac2
Necip Dirik <necipdirik@gmail.com>
2024-03-29 09:34:01 +0300

    custom nodejs server added

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const userName = 'Necip';
-console.log(userName);
+const http = require('http');
+
+function handleRequest(request, response) {
+  response.statusCode = 200;
+  response.end('<h1>Hello World!</h1>');
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
