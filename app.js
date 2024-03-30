27fcc5fbd25e249767e4cb040b78458536ddd317
Necip Dirik <necipdirik@gmail.com>
2024-03-30 18:05:07 +0300

    nodemon added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/currenttime', function (req, res) {
 app.get('/', function (req, res) {
   // res.send('<h1>Hello World!</h1>');
   res.send(
-    '<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>'
+    '<form action="/store-user" method="POST"><label>Your Name: </label><input type="text" name="username"><button>Submit</button></form>'
   );
 }); // localhost:3000/
 
